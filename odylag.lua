4b7e666c06a01672e1d979c2ea97d65114e572b7
Ian Anderson <iananderson@berkeley.edu>
2021-02-14 21:17:46 -0800

    Update odylag.lua

--- odylag.lua
+++ odylag.lua
@@ -1,9 +1,7 @@
 --[[Copyright © 2021, Arico
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +10,6 @@ modification, are permitted provided that the following conditions are met:
     * Neither the name of <addon name> nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -38,12 +35,15 @@ packets = require('packets')
 spell_list = 0 
 
 windower.register_event('incoming chunk', function(id, original, modified, injected, blocked)
-    if (windower.ffxi.get_info().zone == 298 or windower.ffxi.get_info().zone == 279) and id == 0x0AA then 
-		spell_list = spell_list + 1
-			if spell_list > 2 then
-				return true
-			end
-    end
+    if windower.ffxi.get_info().zone == 279 then 
+		if id == 0x0AA then 
+			spell_list = spell_list + 1
+				if spell_list and spell_list > 2 then
+					return true
+				end
+		end
+	end
+	
 end)
 
 windower.register_event('zone change', function()
